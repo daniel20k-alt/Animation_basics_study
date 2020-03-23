828097fb5a9463352bb56381563d0848f733cb0a
Testing_author <testing_beta@mail.com>
2020-03-23 14:49:32 +0200

    Added the UIView.animate switch cases

--- Animation_basics/ViewController.swift
+++ Animation_basics/ViewController.swift
@@ -28,7 +28,25 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
             switch self.currentAnimation {
             case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
                 break
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -200, y: -200)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .yellow
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+                
             default:
                 break
             }
