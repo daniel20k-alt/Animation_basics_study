d8d7fa86d0fb54ef3d7b01c87e235e64431b58de
Testing_author <testing_beta@mail.com>
2020-03-23 13:43:21 +0200

    Creating the structure of the application

--- Animation_basics/ViewController.swift
+++ Animation_basics/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "meerkatJosh"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
-
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+    
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+ 
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
 }
 
